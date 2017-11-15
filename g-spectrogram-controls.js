53726d307bd23b9f6c280013a544f895ab0c71f3
mhrice <matthewrice11@yahoo.com>
2017-11-15 15:38:30 -0800

    Redesigning controls

--- g-spectrogram-controls.js
+++ g-spectrogram-controls.js
@@ -11,29 +11,54 @@ Polymer('g-spectrogram-controls', {
   scaleChoice: 0,
   fadeInTime: 10,
   fadeOutTime: 10,
-  reset: false,
-
-    // console.log("RESET")
-    // this.log= false;
-    // this.labels= true;
-    // this.ticks= 5;
-    // this.pause= false;
-    // this.gain= 3;
-    // this.resolutionMax= 20000;
-    // this.resolutionMin= 20;
-    // this.square= false;
-    // this.scale=false;
-    // this.scaleChoice= 0;
-    // this.fadeInTime= 10;
-    // this.fadeOutTime= 10;
-  // },
+  show1: false,
+  show2: false,
+  label: "Sound Off",
+  sound: 0,
 
   created: function() {
     console.log('Created spectrogram controls');
   },
 
-  resetChanged: function() {
-    console.log("CHANGED");
+  clickAction1: function(e) {
+    var t = e.target;
+    if (this.show1) {
+      t.children[0].classList.remove("up");
+      t.children[0].classList.add("down");
+      this.show1 = false;
+    } else {
+      t.children[0].classList.remove("down");
+      t.children[0].classList.add("up");
+      this.show1 = true;
+    }
+
+  },
+
+  clickAction2: function(e) {
+    var t = e.target;
+    if (this.show2) {
+      t.children[0].classList.remove("up");
+      t.children[0].classList.add("down");
+      this.show2 = false;
+    } else {
+      t.children[0].classList.remove("down");
+      t.children[0].classList.add("up");
+      this.show2 = true;
+    }
+
+  },
+
+  soundToggle: function(e) {
+    var t = e.target;
+    t.classList.toggle("green");
+    if (this.label === "Sound Off") {
+      this.label = "Sound On!";
+      this.sound = 1;
+    } else {
+      this.label = "Sound Off";
+      this.sound = 0;
+    }
+
   }
 
 });
