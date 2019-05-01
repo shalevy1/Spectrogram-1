13ad02b26897bbc2d277259a7fba6f4abf0506aa
mhrice <matthewrice11@yahoo.com>
2019-05-01 00:02:58 -0700

    Added react-router-transition

--- src/App.js
+++ src/App.js
@@ -2,21 +2,58 @@ import React, { Component } from 'react';
 import './App.css';
 import SpectrogramContainer from './components/spectrogram-container';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { spring, AnimatedSwitch } from 'react-router-transition';
 
 function SignalGenerator(props){
   return "Signal Generator";
 }
 
+const pageTransitions = {
+  atEnter: {
+    offset: 200,
+    opacity: 0,
+  },
+  atLeave: {
+    offset: glide(-100),
+    opacity: glide(0),
+  },
+  atActive: {
+    offset: glide(0),
+    opacity: glide(1),
+  },
+};
+
+function mapStyles(styles) {
+  return {
+    opacity: styles.opacity,
+    transform: `translateX(${styles.offset}px)`,
+    height: "100vh"
+  };
+}
+
+function glide(val) {
+  return spring(val, {
+    stiffness: 174,
+    damping: 19,
+  });
+}
+
+
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          {/* <SpectrogramContainer />         */}
-        <Route path="/" exact component={SpectrogramContainer} />
-        <Route path="/SignalGenerator/" component={SignalGenerator} />
-        </div>
-      </Router>
+      <div className="App">
+        <Router>
+          <AnimatedSwitch
+          {...pageTransitions}
+          mapStyles={mapStyles}
+          className="switch-wrapper">
+            {/* <SpectrogramContainer />         */}
+            <Route path="/" exact component={SpectrogramContainer} />
+            <Route path="/SignalGenerator/" component={SignalGenerator} />
+          </AnimatedSwitch>
+        </Router>
+      </div>
     )
   }
 }
